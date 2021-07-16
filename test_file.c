d4b1d02b8821bb9f30bcaae9286751db15d04140
johan_mitma12 <avira365@hotmail.com>
2021-07-16 01:54:27 -0500

    aqui esta mi parte version 1
    esta es lo que he avanzado

--- test_file.c
+++ test_file.c
@@ -1,32 +1,20 @@
 #include<stdio.h>
 #define NUM_PASOS 1000
-
-void message(){
-  printf("Interesante,estoy aportando al codigo");
-
-}
-void addFunctrion(){//esta funcion esta mal esfcrietg asf udegtanergnekrg
- printf()<""<resiste a todos los cambios por favor ");
-}
-void mostrar_vacas(){
-  printf("Mostrar numero vacas");
-}
-int main(int argc, char ** argv){
-  int u=0;
-  for(u=0;u<NUM_PASOS;u++){
-    printf("Los pasos son:%i"+u);
-    //esto es lo que faltaba hermano 
-  }
-  while(false){
-   os.system("shittoria -s -f -t0jjkjk0"); 
-  }
-  //esta agregacion solo sepuede hacer una vez
+void hola mundo(){
+  jefjhwfjhejfhejrfej aler 
   
-  /**ahora estoy aquiiiiiiiiiiiii/
   
-  hola mundo como estas?
   
-  //acabo de realizar este cambio en mi repositorio remoto. nadie mas escriba en esta linea.
-  esta bien ya naide modificara esta linea
-  Linea agregada por johan
+  
+  
+  
+  refrferfer
+  ferferfer
+  
+  
+  
+  
+  egegrgrtgrtgrtgr
+  ttp
+}
 }
